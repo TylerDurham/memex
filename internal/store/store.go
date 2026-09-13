82d4bb3daf709c4e6315ca19e7424e6e3629b1a4
Tyler Durham <2191002+TylerDurham@users.noreply.github.com>
2026-09-12 22:00:42 -0500

    feat(store): add uri column to files table

--- internal/store/store.go
+++ internal/store/store.go
@@ -91,6 +91,7 @@ func (s *Store) migrate() error {
 		CREATE TABLE IF NOT EXISTS files (
 			file_path     TEXT PRIMARY KEY,
 			application   TEXT,
+			uri TEXT,
 			content_hash  TEXT NOT NULL,
 			mod_time      INTEGER NOT NULL,
 			indexed_at    INTEGER NOT NULL
